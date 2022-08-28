a47b926c44dae79742fdd75031df324d57c56a36
ink4tex <jimmy.j.guerra22@gmail.com>
2022-08-27 21:41:27 -0400

    Agregando el .env para las variables de entorno en un archivo

--- app/schema.py
+++ app/schema.py
@@ -1,11 +1,11 @@
 instructions = [
-    'DROP TABLE IF EXITS email;',
+    'DROP TABLE IF EXISTS email;',
     """
         CREATE TABLE email(
             id SERIAL PRIMARY KEY,
             emial TEXT NOT NULL,
             subject TEXT NOT NULL,
-            content TEXR NOT NULL
+            content TEXt NOT NULL
         )
     """
 ]
