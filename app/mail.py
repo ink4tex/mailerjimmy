307ad4fb7f162d6e9a5546e265a5ee5c53dbfd43
ink4tex <jimmy.j.guerra22@gmail.com>
2022-08-28 00:22:14 -0400

    Agregando los estilos y codigo para mostrar los correos registrados en la BDD

--- app/mail.py
+++ app/mail.py
@@ -1,9 +1,14 @@
 from flask import(
     Blueprint, render_template
 )
+from app.db import get_db
 
 bp = Blueprint('mail', __name__, url_prefix="/") #el url_prefix es para poner una ruta antes de la ruta que se quiere llegar ejemplo, https:hola/modulo, el hola/, seria el url_prefix se mostrara en todas las url
 
 @bp.route('/', methods=['GET'])
 def index():
-    return render_template('mails/index.html')
+    db, c = get_db()
+    c.execute("SELECT * FROM email")
+    mails = c.fetchall()
+    
+    return render_template('mails/index.html', mails=mails)
