f184ce36dd40ec31b60672fcdeda3c21ca84e29c
ink4tex <jimmy.j.guerra22@gmail.com>
2022-08-27 20:36:08 -0400

    Creando el script de la BDD en schema.py

--- app/schema.py
+++ app/schema.py
@@ -1,3 +1,11 @@
-instructions = {
-    
-}
+instructions = [
+    'DROP TABLE IF EXITS email;',
+    """
+        CREATE TABLE email(
+            id SERIAL PRIMARY KEY,
+            emial TEXT NOT NULL,
+            subject TEXT NOT NULL,
+            content TEXR NOT NULL
+        )
+    """
+]
