0d8daceb6111f6af660a63b558000c20444ec144
ink4tex <jimmy.j.guerra22@gmail.com>
2022-08-27 20:29:07 -0400

    Creando La Conexion a la BDD

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,20 @@
+import os
 from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_mapping(  #Esto es para saber que llaves se tiene que utilizar en la aplicacion y para obtener las variables de entorno
+        SENDGRID_KEY = os.environ.get('SENDGRID_API_KEY'), #SENDGRID_KEY Es una API de servicio de correos que es gratis, deja mandar 100 correos al dia.. Esta Variable que va a obtener el valor que posee el sistema operativo
+        SECRET_KEY = os.environ.get('SECRET_KEY'),
+        DATABASE_HOST = os.environ.get('FLASK_DATABASE_HOST'),
+        DATABASE_PORT = os.environ.get('FLASK_DATABASE_PORT'),
+        DATABASE_USER = os.environ.get('FLASK_DATABASE_USER'),
+        DATABASE_PASSWORD = os.environ.get('FLASK_DATABASE_PASSWORD'),
+        DATABASE = os.environ.get('FLASK_DATABASE')
+    )
+    
+    from . import db
+    db.init_app(app)
+
+    return app
---
+++ app/db.py
@@ -0,0 +1,43 @@
+from calendar import c
+import psycopg2
+import psycopg2.extras
+import click
+
+from flask import current_app
+from flask import g
+from flask.cli import with_appcontext
+from .schema import instructions
+
+def get_db():
+    if 'db' not in g:
+        g.db = psycopg2.connect(
+            host = current_app.config['DATABASE_HOST'],
+            port = current_app.config['DATABASE_PORT'],
+            user = current_app.config['DATABASE_USER'],
+            password = current_app.config['DATABASE_PASSWORD'],
+            database = current_app.config['DATABASE']
+        )
+        g.c = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    return g.db, g.c
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    db,c = get_db()
+
+    for i in instructions:
+        c.execute(i)
+    db.commit()
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    init_db()
+    click.echo('Base de Datos Inicializada')
+    
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
